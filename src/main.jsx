8d65cc4e09905aedbbb960e572ae3b176ee50d5b
leonardo-da <cruz.leonardodaniel.284ags@gmail.com>
2023-10-31 22:25:43 -0600

    styles correction

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from "./pages/home.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <div className="App">
       <BrowserRouter>
         <Routes>
             <Route exact index element={<App/>}/>
             <Route exact path='home' element={<Home/>}/>
         </Routes>
       </BrowserRouter>
-    </div>
-  </React.StrictMode>,
 )
