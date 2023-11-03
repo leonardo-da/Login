3ceb307ca8be110ddf1d7fb1adb651d745d67123
Javier Guzmán Cruz <skullowner83@gmail.com>
2023-11-03 00:58:51 -0600

    fix problem with styles after installing sass

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import ImgAccount from "../img/Account.svg";
 import ImgPassword from "../img/Password.svg";
+import ImgBackground from "../img/background.gif";
 
 export default function Login() {
     const navigate = useNavigate();
@@ -11,40 +12,44 @@ export default function Login() {
     const [password, setPassword] = useState('');
 
     const handleSubmit = (event) => {
-    event.preventDefault();
-    if(!email || !password){
-      console.log("te falta un dato galan")
-    }else{ 
-      console.log("Informacion enviada")
-    }
+        event.preventDefault();
+        
+        if(!email || !password){
+        console.log("te falta un dato galan");
+        } else { 
+        console.log("Informacion enviada")
+        }
     };
 
     //HTML section
     return (
-    <div className="LogIn-Body Centered-Container">
-        <div className="LogIn-Container">
-            <div className="Title-Container Centered-Container">
-                <label className="Title">Inicio de sesion</label>
+    <div className="LogIn-Page Centered-Container">
+        <div className="Bakground"/>
+
+        <div className="Form-Container">
+            <div className="Centered-Container">
+                <h1>Inicio de sesion</h1>
             </div>
                 
             <form className="LogInForm-Container Centered-Container" onSubmit={handleSubmit}>
                 {/* Account textbox */}
                 <div className="TextBox-Container">
-                    <img className="TextBox-Image" src={ImgAccount} alt="ImgAccount"/>
+                    <img src={ImgAccount} alt="ImgAccount"/>
                     <input className="TextBox" type="email" placeholder="Correo electronico" name="email" /* value={email}  onChange={(event) => setEmail(event.target.value)}*/ />
                 </div>
 
                 {/* Password textbox */}
                 <div className="TextBox-Container">
-                    <img className="TextBox-Image" src={ImgPassword} alt="ImgPassword"/>
+                    <img src={ImgPassword} alt="ImgPassword"/>
                     <input className="TextBox" type="password" placeholder="Contraseña" name="password"  /*value={password} onChange={(event) => setPassword(event.target.value)}*/ />
                 </div>
                 
                 <button className="Button" type="submit" >Iniciar sesion</button>
             </form>
                 
+            {/* Submit button */}
             <div className="FormButtons-Container Centered-Container">
-                <button className="TextButton" onClick={() => navigate("/home")}>olvide contraseña</button>
+                <a onClick={() => navigate("/home")}>olvide contraseña</a>
             </div>
         </div>
     </div>
